e89503e840355bd4041c11c28b2d6543cd7329de
imhilla <hillaryodhiambo@gmail.com>
2021-04-14 07:48:46 +0300

    configure store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import styled from 'styled-components';
 import Credit from './Components/credit';
 import OrderContainer from './Components/orderContainer';
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.jpg';
 
 function App() {
+  const mydata = {
+    cart: [
+      {
+        id: 1,
+        name: "Alex Vando Mens Dress Shirts",
+        subinformation: "Good quality construction, Easy care: This dress shirt is nicely stitched and detailed with 100% hypoallergenic synthetic filling.",
+        price: "$23.99",
+        url: image1
+      },
+      {
+        id: 2,
+        name: "Spalding NBA Street Outdoor Basketball",
+        subinformation: "Performance: Durable outdoor rubber cover. Superior control: Deep channel design for easy handles",
+        price: "$25.00",
+        url: image2
+      },
+      {
+        id: 3,
+        name: "HP Chromebook 14-inch HD Laptop, Intel Celeron N4000.",
+        subinformation: "Google play store: The millions of Android apps you know and love on your phone and tablet can now run on your Chrome device without compromising their speed.",
+        price: "$59.11",
+        url: image3
+      },
+      {
+        id: 4,
+        name: "Chic Home Zarah 10 Piece Comforter Bedding with Sheet Set and Decorative Pillows Shams.",
+        subinformation: "The Zarah queen size patchwork comforter is stuffed with 100% hypoallergenic synthetic filling.",
+        price: "$45.60",
+        url: image4
+      }
+    ]
+  }
   return (
     <Wrapper className="App">
-      <OrderContainer />
+      <OrderContainer mydata={mydata} />
       <Credit />
     </Wrapper>
   );
--- src/Components/orderContainer.js
+++ src/Components/orderContainer.js
@@ -2,14 +2,10 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Cart } from '@styled-icons/boxicons-regular/Cart';
 import Items from './Items';
-import image1 from '../images/image1.jpg';
-import image2 from '../images/image2.jpg';
-import image3 from '../images/image3.jpg';
-import image4 from '../images/image4.jpg';
 import Total from './Total';
 
 
-function OrderContainer() {
+function OrderContainer({ mydata }) {
   const [data, setData] = useState(0)
   const [subtotal, setSubtotal] = useState(0)
   const [total, setTotal] = useState(0)
@@ -17,38 +13,6 @@ function OrderContainer() {
   const [shipping, setShipping] = useState(3.23)
 
   useEffect(() => {
-    const mydata = {
-      cart: [
-        {
-          id: 1,
-          name: "Alex Vando Mens Dress Shirts",
-          subinformation: "Good quality construction, Easy care: This dress shirt is nicely stitched and detailed with 100% hypoallergenic synthetic filling.",
-          price: "$23.99",
-          url: image1
-        },
-        {
-          id: 2,
-          name: "Spalding NBA Street Outdoor Basketball",
-          subinformation: "Performance: Durable outdoor rubber cover. Superior control: Deep channel design for easy handles",
-          price: "$25.00",
-          url: image2
-        },
-        {
-          id: 3,
-          name: "HP Chromebook 14-inch HD Laptop, Intel Celeron N4000.",
-          subinformation: "Google play store: The millions of Android apps you know and love on your phone and tablet can now run on your Chrome device without compromising their speed.",
-          price: "$59.11",
-          url: image3
-        },
-        {
-          id: 4,
-          name: "Chic Home Zarah 10 Piece Comforter Bedding with Sheet Set and Decorative Pillows Shams.",
-          subinformation: "The Zarah queen size patchwork comforter is stuffed with 100% hypoallergenic synthetic filling.",
-          price: "$45.60",
-          url: image4
-        }
-      ]
-    }
     setData(mydata);
   }, []);
 
@@ -63,7 +27,7 @@ function OrderContainer() {
       return accumulator + element;
     }, 0);
     setSubtotal(sum)
-  })
+  }, [data])
 
   const items = data ? (
     <div>
